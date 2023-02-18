86b1ab2345e3bafeb58b79be3cb305698ca915e4
Thiago Yugue Kono Campos <106500654+kono100@users.noreply.github.com>
2023-02-18 17:25:15 -0300

    Add files via upload

---
+++ 4ex Salario+25%.py
@@ -0,0 +1,14 @@
+
+
+
+
+#4. Faça um programa que receba o salário de um funcionário, calcule e mostre o novo salário,
+#sabendo-se que este sofreu um aumento de 25%.
+
+Salario = int(input("Digite o seu Salario: "))
+
+Salario1=(Salario*25/100)
+
+TOTAL = (Salario+Salario1)
+
+print(f"\n25% do Seu salario é R${Salario1} e o TOTAL:R${TOTAL}\n")
